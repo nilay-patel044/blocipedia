a028d138551a4170afc821436a9b119dcc304e05
nilay-patel044 <nilaypatel044@gmail.com>
2017-12-03 01:54:17 -0500

    Blocipedia-9: Completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,28 @@ module ApplicationHelper
     css_class << ' has-error' if errors.any?
     content_tag :div, capture(&block), class: css_class
   end
+
+  def markdown(text)
+    options = {
+      filter_html: true,
+      hard_wrap: true,
+      link_attributes: { rel: 'nofollow', target: "_blank" },
+      space_after_headers: true,
+      fenced_code_blocks: true
+    }
+
+    extensions = {
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      autolink: false,
+      strikethrough: true,
+      superscript: true,
+      disable_indented_code_blocks: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    return markdown.render(text).html_safe
+  end
 end
